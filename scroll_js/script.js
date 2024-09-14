486006b0f3552808239755ff585e43ca5cc94bc7
Prabin Karki <karkiprabin198@gmail.com>
2024-09-14 21:48:27 +0545

    scroll js

---
+++ scroll_js/script.js
@@ -0,0 +1,17 @@
+const scrollContainer=document.querySelector('.gallary');
+const backBtn=document.getElementById('backBtn');
+const nextBtn=document.getElementById('nextBtn');
+scrollContainer.addEventListener('wheel',(evt)=>{
+         evt.preventDefault();
+         scrollContainer.scrollLeft +=evt.deltaY;
+         scrollContainer.style.scrollBehavior="auto";
+})
+backBtn.addEventListener('click',()=>{
+    scrollContainer.scrollLeft -=900;
+    
+
+})
+nextBtn.addEventListener('click',()=>{
+    scrollContainer.scrollLeft +=900;
+    
+})
